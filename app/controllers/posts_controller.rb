5553d0a938194275c5917781138adcc749b68d48
tranquangvu <vutran.513@gmail.com>
2017-08-13 09:53:23 +0700

    Add API for creating post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
   def index
     render json: { posts: ['post1', 'post2'] }, status: 200
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      render json: @post, status: 201
+    else
+      render json: { errors: @post.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:caption, :photo)
+  end
 end
