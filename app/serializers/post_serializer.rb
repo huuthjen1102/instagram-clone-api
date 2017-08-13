5df432df6b72c36c61d89170170ec8087024822a
tranquangvu <vutran.513@gmail.com>
2017-08-13 09:57:50 +0700

    Add filter to post

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  caption    :text
+#  photo      :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  filter     :string
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :photo, :caption
 end
